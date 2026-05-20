d3b2b937aaf1ee3f2037943bcb6aabdc12dd5828
bharath <coludstier16@gmail.com>
2026-05-20 15:43:24 +0530

    prj1 delete

--- Decision_Engine/urls.py
+++ Decision_Engine/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('workflow/', include('workflow.urls')),
     path('', include('Core.urls')),
 ]
     
